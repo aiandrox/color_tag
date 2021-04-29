280ce41da55131f5cbf59eb04a0f1442dd23b9a5
Aiandrox <aiandrox7@gmail.com>
2021-04-29 23:51:21 +0900

    [Add] Canvasで画像を表示 https://qiita.com/shikigamix/items/fe862b6f86146aa8dfc0

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,41 @@ function App() {
     getColors();
   }, []);
 
+  // contextを状態として持つ
+  const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
+  // 画像読み込み完了トリガー
+  const [loaded, setLoaded] = useState(false);
+  // コンポーネントの初期化完了後コンポーネント状態にコンテキストを登録
+  useEffect(() => {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    const canvasContext = canvas.getContext("2d");
+    setContext(canvasContext);
+  }, []);
+
+  useEffect(() => {
+    if (context !== null) {
+      const img = new Image();
+      img.src = "images/cherry.jpg"; // 描画する画像など
+      img.onload = () => {
+        context.drawImage(img, 0, 0);
+        // 更にこれに続いて何か処理をしたい場合
+        setLoaded(true);
+      };
+    }
+  }, [context]);
+
+  useEffect(() => {
+    if (loaded) {
+      // それに続く処理
+    }
+  }, [loaded]);
+
   return (
     <div className="App">
       <Container
         maxWidth="sm"
         style={{
-          height: "100vh",
+          // height: "100vh",
           justifyContent: "center",
           display: "flex",
           flexDirection: "column",
@@ -32,6 +61,7 @@ function App() {
         }}
       >
         <h1>いろおに</h1>
+        <canvas width="880" height="520" id="canvas"></canvas>
         <div>
           {pictureColors.map((color) => {
             return (
@@ -48,7 +78,12 @@ function App() {
             );
           })}
         </div>
-        <CardMedia component="img" image="images/cherry.jpg" title="App-logo" />
+        <CardMedia
+          component="img"
+          image="images/cherry.jpg"
+          title="image"
+          id="image"
+        />
         <Top></Top>
       </Container>
     </div>
