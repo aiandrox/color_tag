ab585d7da79712307cb9dd72a39fa3866c914ee3
Aiandrox <aiandrox7@gmail.com>
2021-05-05 10:46:45 +0900

    [Add] トップロゴ

--- src/Top.tsx
+++ src/Top.tsx
@@ -1,16 +1,37 @@
-import { Button } from "rebass";
+import { Button, Text } from "rebass";
 
 type TopProps = {
   clickStart: () => void;
 };
 
 const Top = (props: TopProps) => {
+  const IrooniStyle = {
+    color: "#FF00A1",
+    background: "-webkit-linear-gradient(-90deg, #FF00A1, #F6FF00)",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+    fontFamily: "'M PLUS Rounded 1c', sans-serif",
+  };
+
   return (
     <div>
       <h1>
-        <span>アルティメット</span>いろおに
+        <Text
+          fontSize={8}
+          color="#63009c"
+          style={{ fontFamily: "'Reggae One', cursive" }}
+        >
+          アルティメット
+        </Text>
+        <Text fontSize={8} style={IrooniStyle}>
+          いろおに
+        </Text>
       </h1>
-      <Button backgroundColor="#000000" onClick={props.clickStart}>
+      <Button
+        backgroundColor="#ffc240"
+        onClick={props.clickStart}
+        style={{ cursor: "pointer" }}
+      >
         いろいろなーにいろ？
       </Button>
     </div>
