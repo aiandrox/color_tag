56b29f006faf830c5e9e9d78f936911e5feadfb1
Aiandrox <aiandrox7@gmail.com>
2021-05-15 01:09:35 +0900

    [Style] エクストラへの動線をわかりやすく

--- src/Clear.tsx
+++ src/Clear.tsx
@@ -144,7 +144,7 @@ const Clear = () => {
             history.push(`/game?mode=extra`);
           }}
         >
-          EXTRA
+          エクストラモード
         </CustomButton>
       </Box>
     </Box>
